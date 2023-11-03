4c713f48af0f96f4a830af9184182b871a8ba619
matryt <mathieu.cuvelier@outlook.fr>
2023-11-03 17:08:33 +0100

    Modifications logique

--- additifs.py
+++ additifs.py
@@ -6,9 +6,12 @@ def trouver_additifs(texte):
     texte=str(texte)
     return re.findall("[eE][0-9]{3}", texte)
 
-def trouver_information_additifs(listeAdditifs):
+def infos(listeAdditifs):
     for i in listeAdditifs:
-        url="https://fr.openfoodfacts.org/additif/E" + i[1:]
-        print('Une page d\'information sur l\'additif E', i[1:], ' a été ouverte dans le navigateur')
-        webbrowser.open(url)
-        time.sleep(1)
+        print("Le produit contient l'additif E", i[1:])
+        consent = input('Voulez-vous en savoir plus sur l\'additif E' + i[1:] + ' ? (o/n) ')
+        if consent == 'o':
+            url = f"https://fr.openfoodfacts.org/additif/E{i[1:]}"
+            print('Une page d\'information sur l\'additif E', i[1:], ' a été ouverte dans le navigateur')
+            webbrowser.open(url)
+            time.sleep(1)
--- extraction_données_produit_alimentaire_par_code-barre.py
+++ extraction_données_produit_alimentaire_par_code-barre.py
@@ -1,121 +1,69 @@
-import numpy as np
 from requests import get as obtenir
 import additifs
-import urllib.request
-import cv2
 
-def image_from_url(url):
-    rep = urllib.request.urlopen(url).read()
-    image = np.asarray(bytearray(rep), dtype="uint8")
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    return image
+quantitesEnToFrench = {
+    "low": "faible",
+    "moderate": "moyen",
+    "high": "élevé",
+    None: "non indiqué"
+}
 
+def getJson(code):
+    url = f'https://world.openfoodfacts.org/api/v0/product/{code}.json'
+    try:
+        statut = obtenir(url)
+        donnees = statut.json()
+        if len(donnees["code"]) == 0:
+            print('Ce produit n\'est pas connu')
+            return None
+    except ValueError:
+        return None
+    return donnees
+
+def getAllData(donnees):
+    product = donnees.get("product")
+    nom = product.get("product_name_fr") or "Inconnu"
+    marque = product.get("brands_imported") or product.get("brands") or "Inconnu"
+    preparation = product.get("preparation") or "Inconnu"
+    quantite = product.get("quantity") or "Inconnu"
+    vegan = "Oui" if product.get("ingredients")[0].get("vegan") else "Non"
+    vegetarien = "Oui" if product.get("ingredients")[0].get("vegetarian") else "Non"
+    ingredients = product.get("ingredients_text_fr") or "Inconnu"
+    sucres = quantitesEnToFrench[product.get("nutrient_levels").get("sugars")]
+    graissesSaturees = quantitesEnToFrench[product.get("nutrient_levels").get("saturated-fat")]
+    sel = quantitesEnToFrench[product.get("nutrient_levels").get("salt")]
+    nutriscore = product.get("nutrition_grade_fr").upper() if product.get("nutrition_grade_fr") else "Inconnu"
+    return nom, marque, preparation, quantite, vegan, vegetarien, ingredients, sucres, graissesSaturees, sel, nutriscore
 
 while True:
-    code_barre=input('Quel est le code barre ? ')
-    url='https://world.openfoodfacts.org/api/v0/product/'+code_barre+'.json'
-    try:
-        statut=obtenir(url)
-        données=statut.json()
-        if len(données["code"])==0:
-            print('Ce produit n\'est pas connu')
-            continue
-    except ValueError:
-        pass
-    try:
-        print('Le nom du produit est : ', données["product"]["product_name_fr"], "\n")
-    except KeyError:
-        print('Le nom de ce produit n\'est pas donné\n')
+    donnees = getJson(input('Entrez le code-barre du produit : '))
+    if donnees is None:
+        continue
+    nom, marque, preparation, quantite, vegan, vegetarien, ingredients, sucres, graissesSaturees, sel, nutriscore = getAllData(donnees)
+    print(f'''
+        Nom : {nom}
+        Marque : {marque}
+        Préparation : {preparation}
+        Quantité : {quantite}
+        Vegan : {vegan}
+        Végétarien : {vegetarien}
+        Ingrédients : {ingredients}
+        Sucres : {sucres}
+        Graisses saturées : {graissesSaturees}
+        Sel : {sel}
+        Nutriscore : {nutriscore}
+        ''')
 
-    try:
-        print('La marque du produit est : ', données["product"]["brands_imported"], "\n")
-    except KeyError:
-        try:
-            print('La marque du produit est : ', données["product"]["brands"], "\n")
-        except:
-            print('La marque du produit n\'est pas donnée \n')
-
-    try:
-        print('La préparation de ce produit est : ', données["product"]["preparation"], "\n")
-    except KeyError:
-        print('La préparation de ce produit n\'est pas donnée\n')
-
-    try:
-        print('La quantité de ce produit est : ', données["product"]["quantity"], "\n")
-    except KeyError:
-        print('La quantité de ce produit n\'est pas donnée\n')
-
-    try:
-        print('Ce produit est vegan  : ', 'oui' if données["product"]["ingredients"][0]["vegan"]=='yes' else 'non', "\n")
-    except (KeyError, IndexError) as e:
-        print('Le caractère vegan ou non de ce produit n\'est pas indiqué\n')
-
-    try:
-        print('Ce produit est végétarien  : ', 'oui' if données["product"]["ingredients"][0]["vegetarian"]=='yes' else 'non', "\n")
-    except (KeyError, IndexError) as e:
-        print('Le caractère végétarien ou non de ce produit n\'est pas indiqué\n')
-
-    print('Les ingrédients précisés entre underscores (_) sont des allergènes potentiels')
-    try:
-        print('Ingrédients du produit  : ', données["product"]["ingredients_text_fr"], "\n")
-    except KeyError:
-        print('Les ingrédients de ce produit ne sont pas indiqués\n')
-
-    try:
-        if données["product"]["nutrient_levels"]["sugars"]=='low':
-            print('Apport en sucres : faible\n')
-        elif données["product"]["nutrient_levels"]["sugars"]=='moderate':
-            print('Apport en sucres : moyen\n')
+    listeAdditifs = donnees["product"]["additives_tags"]
+    ingredients = donnees["product"].get("ingredients_text_fr")
+    infos = True
+    if listeAdditifs is None:
+        if ingredients is None:
+            infos = False
+            print("Aucun information d'additif")
         else:
-            print('Apport en sucres : élevé\n')
-    except KeyError:
-        print('L\'apport en sucres de ce produit n\'est pas indiqué\n')
-
-    try:
-        if données["product"]["nutrient_levels"]["saturated-fat"]=='low':
-            print('Apport en graisses saturées : faible\n')
-        elif données["product"]["nutrient_levels"]["saturated-fat"]=='moderate':
-            print('Apport en graisses saturées : moyen\n')
-        else:
-            print('Apport en graisses saturées : élevé\n')
-    except KeyError:
-        print('L\'apport en graisses saturées de ce produit n\'est pas indiqué\n')
-
-    try:
-        if données["product"]["nutrient_levels"]["salt"]=='low':
-            print('Apport en sel : faible\n')
-        elif données["product"]["nutrient_levels"]["salt"]=='moderate':
-            print('Apport en sel : moyen\n')
-        else:
-            print('Apport en sel : élevé\n')
-    except KeyError:
-        print('L\'apport en sel de ce produit n\'est pas indiqué\n')
-
-    try:
-        if données["product"]["nutrient_levels"]["fat"]=='low':
-            print('Apport en graisses : faible\n')
-        elif données["product"]["nutrient_levels"]["fat"]=='moderate':
-            print('Apport en graisses : moyen\n')
-        else:
-            print('Apport en graisses : élevé\n')
-    except KeyError:
-        print('L\'apport en graisses de ce produit n\'est pas indiqué\n')
-
-    try:
-        print('Nutriscore du produit  : ', données["product"]["nutrition_grade_fr"].upper(), '\n')
-    except KeyError:
-        print('Le Nutriscore de ce produit n\'est pas indiqué\n')
-
-    try:
-        a=additifs.trouver_additifs(données["product"]["ingredients_text_fr"])
-        b=additifs.trouver_information_additifs(a)
-        if a==[]:
-            repertoire=données["product"]["additives_tags"]
-            for i in repertoire:
-                add=i[4:]
-                additif=additifs.trouver_additifs(i)
-                b=additifs.trouver_information_additifs(additif)
-    except KeyError:
-        pass
-
-    break
+            listeAdditifs = additifs.trouver_additifs(ingredients)
+    if infos and len(listeAdditifs) > 0:
+        additifs.infos(listeAdditifs)
+    else:
+        print("Aucun additif")
